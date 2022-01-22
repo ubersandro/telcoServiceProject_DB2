92b5f3e80589d63df7f829ac5f49f672c8add5dd
aspo98 <aspo98@icloud.com>
2022-01-22 19:18:49 +0100

    template aggiornati

--- consumerApp/telcoServiceConsumerWEB/src/main/java/controllers/GoToBuyPage.java
+++ consumerApp/telcoServiceConsumerWEB/src/main/java/controllers/GoToBuyPage.java
@@ -32,20 +32,19 @@ public class GoToBuyPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@EJB
 	private ServicePackageService sps;
-	private TemplateEngine templateEngine; 
-	
+	private TemplateEngine templateEngine;
+
 	/**
-	 * REQUEST PARAMETERS: servicePackageID
-	 * RESPONSE : servicePackage, services, optionalProducts, ValidityPeriods 
+	 * REQUEST PARAMETERS: servicePackageID RESPONSE : servicePackage, services,
+	 * optionalProducts, ValidityPeriods
 	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// retrieve the package from the request
-		Integer packageID = Integer.parseInt(req.getParameter("pid")); 
+		Integer packageID = Integer.parseInt(req.getParameter("pid"));
 		ServicePackage sp = sps.findServicePackage(packageID);
-		List<OptionalProduct> opts = sps.findAssociableOptionalProducts(packageID); // TODO insert method on object 
-		
-		
+		List<OptionalProduct> opts = sps.findAssociableOptionalProducts(packageID); // TODO insert method on object
+
 		//
 		//
 		OptionalProduct a = new OptionalProduct("op1", 2.1);
@@ -53,33 +52,36 @@ public class GoToBuyPage extends HttpServlet {
 		OptionalProduct c = new OptionalProduct("op3", 2.3);
 		OptionalProduct d = new OptionalProduct("op4", 2.4);
 		OptionalProduct e = new OptionalProduct("op5", 2.5);
-		
+
 		opts.add(a);
 		opts.add(b);
 		opts.add(c);
 		opts.add(d);
 		opts.add(e);
+
+		sp.addCost(new ValidityPeriod(12), 20);
+		sp.addCost(new ValidityPeriod(24), 4.66);
+		sp.addCost(new ValidityPeriod(36), 2.78);
 		//
 		//
-		
-		
-		//template parameters insertion  
-		String template = "BuyPage"; 
+
+		// template parameters insertion
+		String template = "BuyPage";
 		ServletContext servletContext = getServletContext();
 		final WebContext ctx = new WebContext(req, resp, servletContext, req.getLocale());
-		ctx.setVariable("servicePackage", sp); //String representation with services included 
-		ctx.setVariable("associableOptionalProducts", opts); 
-		// TODO what about validity periods ? 
+		ctx.setVariable("servicePackage", sp); // String representation with services included
+		ctx.setVariable("associableOptionalProducts", opts);
+		// TODO what about validity periods ?
 		templateEngine.process(template, ctx, resp.getWriter());
 	}
 
-	
 	@Override
-	public void destroy() {}
+	public void destroy() {
+	}
 
 	@Override
 	public void init() throws ServletException {
-		templateEngine = ServletUtils.initHelper(this, "WEB-INF/templates/"); 
+		templateEngine = ServletUtils.initHelper(this, "WEB-INF/templates/");
 	}
 
 }
