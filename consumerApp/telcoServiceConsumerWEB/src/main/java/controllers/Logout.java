2e36e51012c124dc466c286911a988b55548d80d
ubersandro <alessandromileto22@gmail.com>
2022-01-25 19:16:42 +0100

    home page employee button fixed, logout added, logout servlet with invalidation

--- consumerApp/telcoServiceConsumerWEB/src/main/java/controllers/Logout.java
+++ consumerApp/telcoServiceConsumerWEB/src/main/java/controllers/Logout.java
@@ -14,6 +14,7 @@ public class Logout extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getSession().removeAttribute("user");
+		req.getSession().invalidate();
 		resp.sendRedirect(this.getServletContext().getContextPath());
 	}
 }
